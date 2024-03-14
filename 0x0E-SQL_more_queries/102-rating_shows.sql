e7e4867fe4d4805908f41bf99cc4641193ca7605
Yinkaxlusive <97adeyinkaonatola@gmail.com>
2024-03-14 08:59:56 +0100

    0x0E-SQL_more_queries latest

--- 0x0E-SQL_more_queries/102-rating_shows.sql
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -1,5 +1,7 @@
-SELECT tv_shows.title, SUM(tv_show_ratings.rating) AS rating_sum
+-- lists all shows from hbtn_0d_tvshows_rate by their rating
+-- lists all rows of a table by the sum of a linked row
+SELECT title, SUM(tv_show_ratings.rate) 'rating'
 FROM tv_shows
-JOIN tv_show_ratings ON tv_shows.id = tv_show_ratings.show_id
-GROUP BY tv_shows.title
-ORDER BY rating_sum DESC;
+LEFT JOIN tv_show_ratings ON tv_show_ratings.show_id = tv_shows.id
+GROUP BY title
+ORDER BY rating DESC;
--- 0x0E-SQL_more_queries/103-rating_genres.sql
+++ 0x0E-SQL_more_queries/103-rating_genres.sql
@@ -1,6 +1,8 @@
-SELECT tg.name AS genre, SUM(ts.rating) AS rating_sum
-FROM tv_genres AS tg
-JOIN tv_show_genres AS tsg ON tg.id = tsg.genre_id
-JOIN tv_shows AS ts ON tsg.show_id = ts.id
-GROUP BY tg.name
-ORDER BY rating_sum DESC;
+-- lists all genres in the database hbtn_0d_tvshows_rate by their rating
+-- lists all rows in a database linked to a row in another table
+SELECT name, SUM(tv_show_ratings.rate) 'rating'
+FROM tv_genres
+INNER JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
+INNER JOIN tv_show_ratings ON tv_show_genres.show_id = tv_show_ratings.show_id
+GROUP BY name
+ORDER BY rating DESC;
