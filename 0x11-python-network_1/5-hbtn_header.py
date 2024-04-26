5b76357a92d98fe051ffdd66b470805194321387
Yinkaxlusive <97adeyinkaonatola@gmail.com>
2024-04-27 00:24:54 +0100

    God oh

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Displays the X-Request-Id header variable of a request to a given URL
+"""A python script that displays the X-Request-Id header variable of a request to a given URL
 """
 import sys
 import requests
