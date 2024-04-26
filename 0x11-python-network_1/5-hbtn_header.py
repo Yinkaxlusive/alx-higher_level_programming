34b85c58306e105ed67ce59f1c5cfbee4159dba3
Yinkaxlusive <97adeyinkaonatola@gmail.com>
2024-04-27 00:33:46 +0100

    5new

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -1,12 +1,20 @@
 #!/usr/bin/python3
 """A python script that displays the X-Request-Id header variable of a request to a given URL
 """
-import sys
 import requests
+import sys
 
+def get_request_id(url):
+    try:
+        response = requests.get(url)
+        request_id = response.headers.get('X-Request-Id')
+        if request_id:
+            print(f"X-Request-Id: {request_id}")
+        else:
+            print("X-Request-Id not found in the response headers.")
+    except requests.exceptions.RequestException as e:
+        print(f"Error: {e}")
 
 if __name__ == "__main__":
     url = sys.argv[1]
-
-    r = requests.get(url)
-    print(r.headers.get("X-Request-Id"))
+    get_request_id(url)
