26d9ceac50d294a797d3291e8c8fed540ce4f2f6
Yinkaxlusive <141252881+Yinkaxlusive@users.noreply.github.com>
2024-04-27 19:55:59 +0100

    Update 100-github_commits.py

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -1,20 +1,24 @@
 #!/usr/bin/python3
-"""lists the 10 most recent commits on a given GitHub repository.
 """
-import sys
+Python script that takes repository name and owner name as arguments
+and lists 10 most recent commits from the repository by the owner
+using the GitHub API.
+Uses the requests and sys packages.
+"""
 import requests
-
+import sys
 
 if __name__ == "__main__":
-    url = "https://api.github.com/repos/{}/{}/commits".format(
-        sys.argv[2], sys.argv[1])
+    if len(sys.argv) != 3:
+        print("Usage: {} <repo> <owner>".format(sys.argv[0]))
+        sys.exit(1)
 
-    r = requests.get(url)
-    commits = r.json()
-    try:
-        for i in range(10):
-            print("{}: {}".format(
-                commits[i].get("sha"),
-                commits[i].get("commit").get("author").get("name")))
-    except IndexError:
-        pass
+    repo_name = sys.argv[1]
+    owner_name = sys.argv[2]
+    url = "https://api.github.com/repos/{}/{}/commits".format(owner_name, repo_name)
+    response = requests.get(url)
+    commits = response.json()
+    for commit in commits[:10]:
+        sha = commit["sha"]
+        author_name = commit["commit"]["author"]["name"]
+        print("{}: {}".format(sha, author_name))
