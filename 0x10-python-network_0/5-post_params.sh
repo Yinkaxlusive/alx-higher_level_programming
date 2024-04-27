260a731f70585ba129c4cea52066b413e805479c
Yinkaxlusive <141252881+Yinkaxlusive@users.noreply.github.com>
2024-04-27 19:30:11 +0100

    Update 5-post_params.sh

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
 # script that takes in a URL, sends a POST request to the passed URL, and displays the body of the response
-# Set POST parameters
-EMAIL="test@gmail.com"
-SUBJECT="I will always be here for PLD"
-curl -s "$1" -X POST -d "email=$EMAIL&subject=$SUBJECT" "$URL"
+curl -s "$1" -X POST -d "email=test@gmail.com&subject=I will always be here for PLD"
