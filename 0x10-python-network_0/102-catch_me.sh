985c86bf159f37f842653e997cece441863a2f85
Yinkaxlusive <141252881+Yinkaxlusive@users.noreply.github.com>
2024-04-27 18:47:19 +0100

    Update 102-catch_me.sh

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-# This script makes a request to 0.0.0.0:5000/catch_me_3 and captures the response.
-curl -sL -X PUT -H "Origin: HolbertonSchool" -d "user_id=98" 0.0.0.0:5000/catch_me
+
+# Make a POST request to the server with a specific header
+curl -s -X POST -H "Origin: HolbertonSchool" -d "user_id=98" 0.0.0.0:5000/catch_me
