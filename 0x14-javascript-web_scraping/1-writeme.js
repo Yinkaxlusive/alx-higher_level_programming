337beb62416ca6c951c2cc65789cd6b30374e1b0
Yinkaxlusive <141252881+Yinkaxlusive@users.noreply.github.com>
2024-05-22 23:57:38 +0100

    Update 1-writeme.js

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,9 +1,8 @@
 #!/usr/bin/node
 const fs = require('fs');
-const file = process.argv[2];
-const content = process.argv[3];
-fs.writeFile(file, content, 'utf-8', function (err) {
-  if (err) {
-    console.log(err);
-  }
+const filePath = process.argv[2];
+const writeData = process.argv[3];
+
+fs.writeFile(filePath, writeData, 'utf-8', (error) => {
+  if (error) { console.log(error); }
 });
